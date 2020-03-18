2bfd0c26235ae2dc7ae0593ff52ec8dd746666ab
jadavjik <jadavjik@ML0250-25>
2020-03-18 11:19:51 +0000

    Add project files.

---
+++ BusinessContact.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactManager8796565
+{
+    class BusinessContact
+    {
+        public string contactBusinessTel { get; set; }
+    }
+}
---
+++ PersonalContact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactManager8796565
+{
+    class PersonalContact
+    {
+        private string contactHomeTel;
+        public string ContactHomeTel
+        {
+            get { return contactHomeTel; }
+            set { contactHomeTel = value; }
+        }
+    }
+}
